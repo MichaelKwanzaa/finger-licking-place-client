f1c64d47882973f61d17d3d8d5207e31d424a4e5
MichaelKwanzaa <mikea340@gmail.com>
2023-07-05 08:56:22 -0500

    current website website july 5th

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'finger-licking-place-client';
+  @ViewChild('juices') public juices!: ElementRef;
+  @ViewChild('menu') public menu!: ElementRef;
+  @ViewChild('contact') public contact!: ElementRef;
+
+  isOpen: boolean = false;
+  
+  constructor(private router: Router){
+    this.isOpen = false;
+  }
+
+  handleMenu(){
+    this.isOpen = !this.isOpen;
+  }
+
+  goHome(){
+    this.router.navigate(["/"]);
+    this.isOpen = false;
+  }
+
+  goToJuices(){
+    this.juices.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'start' });
+    this.isOpen = false;
+
+  }
+
+  goToMenu(){
+    this.menu.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'start' });
+    this.isOpen = false;
+
+  }
+
+  goToContact(){
+    this.contact.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'start' });
+    this.isOpen = false;
+
+  }
 }
